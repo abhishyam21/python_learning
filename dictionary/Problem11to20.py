f8d95188d527dc44411aa5c25af595995d6012c8
Abhishyam <chabhishyam@gmail.com>
2018-05-15 12:20:50 +0530

    Update Problem11to20.py

--- dictionary/Problem11to20.py
+++ dictionary/Problem11to20.py
@@ -1,4 +1,15 @@
 # problem 11
+my_dict = {'data1': 100, 'data2': -54, 'data3': 247}
+
+
+def mul_values1(input):
+    result = 1
+    for key in input:
+        result *= input[key]
+    return result
+
+
+print(mul_values1(my_dict))
 
 print("--------------Problem 11 End--------------------------")
 
@@ -20,12 +31,10 @@ print("--------------Problem 14 End--------------------------")
 
 print("--------------Problem 15 End--------------------------")
 
-
 # problem 16
 
 
 print("--------------Problem 16 End--------------------------")
-
 
 # problem 17
 
