175760339295e3fcb9814e3d1fb3905c7bc116a0
Abhishyam <chabhishyam@gmail.com>
2018-05-15 12:16:04 +0530

    Update Problem1to10.py

--- dictionary/Problem1to10.py
+++ dictionary/Problem1to10.py
@@ -31,28 +31,98 @@ print("--------------Problem 3 End--------------------------")
 
 # problem 4
 
+d = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+
+
+def is_key_present1(input):
+    for i in d.items():
+        if i[0] == input:
+            print("Key already exists")
+            return
+    print("No Key exists")
+
+
+def is_key_present2(input):
+    if input in d:
+        print("Key already exists")
+    else:
+        print("No Key exists")
+
+
+is_key_present1(5)
+is_key_present1(9)
+
+is_key_present2(5)
+is_key_present2(9)
+
 print("--------------Problem 4 End--------------------------")
 
 # problem 5
+
+for key, val in d.items():
+    print("{} --> {}".format(key, val))
+print("-------------------------------")
+for i in d.items():
+    print("{} --> {}".format(i[0], i[1]))
 
 print("--------------Problem 5 End--------------------------")
 
 # problem 6
 
+output = {}
+for i in range(1, 10):
+    output[i] = i ** 2
+print(output)
+
 print("--------------Problem 6 End--------------------------")
 
 # problem 7
 
+
+output = {}
+for i in range(1, 16):
+    output[i] = i * i
+print(output)
+
 print("--------------Problem 7 End--------------------------")
 
 # problem 8
+d1 = {'a': 100, 'b': 200}
+d2 = {'x': 300, 'y': 200}
+d3 = {}
+d3.update(d1)
+d3.update(d2)
+print(d3)
 
 print("--------------Problem 8 End--------------------------")
 
 # problem 9
 
+for key, val in d.items():
+    print("{} --> {}".format(key, val))
+print("-------------------------------")
+for i in d.items():
+    print("{} --> {}".format(i[0], i[1]))
+
 print("--------------Problem 9 End--------------------------")
 
 # problem 10
+
+my_dict = {'data1': 100, 'data2': -54, 'data3': 247}
+
+
+def sum_values1(input):
+    result = 0
+    for key, val in input.items():
+        result += val
+    return result
+
+
+def sum_values2(input):
+    return sum(input.values())
+
+
+print(sum_values1(my_dict))
+print(sum_values2(my_dict))
 
 print("--------------Problem 10 End--------------------------")
